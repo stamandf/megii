84ec24b79629f64688e3cc5da2960619a16cfbba
Florence <stamandf@gmail.com>
2022-04-25 11:18:57 -0400

    Upgrade popper.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/fonts/icomoon/style.css';
 import $ from 'jquery';
-import Popper from 'popper.js';
+import Popper from '@popperjs/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
