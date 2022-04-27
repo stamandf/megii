0cec6a12442ab5d4d911ec9b536543255297f1f1
Florence <stamandf@gmail.com>
2022-04-27 15:38:25 -0400

    Remove mobile menu component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Header from './layouts/Header/Header.jsx';
-import MobileMenu from './layouts/MobileMenu/MobileMenu.jsx';
-import OurTeam from './layouts/OurTeam/OurTeam.jsx';
-import Footer from './layouts/Footer/Footer.jsx';
-import MainCover from './layouts/MainCover/MainCover.jsx';
 import Home from './pages/Home/Home.jsx';
 
 function App() {
