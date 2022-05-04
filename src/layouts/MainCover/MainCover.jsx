8e5ae5d4fc72a8097dadf6623f0134f48a50ecc0
Florence <stamandf@gmail.com>
2022-05-04 10:35:59 -0400

    Add inc. to title.

--- src/layouts/MainCover/MainCover.jsx
+++ src/layouts/MainCover/MainCover.jsx
@@ -18,7 +18,7 @@ function MainCover() {
           <div className="container">
             <div className="row align-items-center justify-content-center text-center">
               <div className="col-md-7">
-                <h1 className="mb-4 text-white">Maison Eros <span className="text-primary">Great</span>ti <br/> Immobilier</h1>
+                <h1 className="mb-4 text-white">Maison Eros <span className="text-primary">Great</span>ti <br/> Immobilier inc.</h1>
                 <div className="d-flex media-38289 justify-content-around mb-5">
                   <p><a href="#" className="btn btn-primary text-white px-4 py-3" onClick={toggleButton}>{buttonPressed ? t("message.button1") : t("message.button2")}</a></p>
                   {/* <p><a href="#" className="btn btn-primary text-white px-4 py-3"onClick={toggleButton}>{buttonPressed ? "Stay Tuned!" : "Under Construction"}</a></p> */}
